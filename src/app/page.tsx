44691975879d6d861307ff554ef071b182f675c3
Rafffan <vanidrisraffan@gmail.com>
2024-09-30 16:36:14 +0700

    finished

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,21 @@
 "use client";
 
+import { PostOwnnerProps } from "@/libs/types"
+import { CommentProps } from "@/libs/types"
+import { ReplyProps } from "@/libs/types"
+import { comments } from "@/libs/comments"
+
+import Commentt from "@/components/Comment";
+
+import PostOwnnerr from "@/components/PostOwnner";
 export default function Home() {
+
+  const postOwner: PostOwnnerProps = 
+  {userImagePath : "/profileImages/handsome.jpg",
+    username : "Raffan Van-idris 660610770" ,
+    commentText : "Quiz ง่ายจังเลยครับ ขอยาก ๆ กว่านี้ได้ไหม #261207" ,
+    likeNum : 100};
+
   return (
     <div
       style={{ minHeight: "100vh", backgroundColor: "#18191A" }}
@@ -12,82 +27,10 @@ export default function Home() {
         className="mx-auto p-3 rounded rounded-3 shadow-sm"
       >
         {/* Post Owner Example*/}
-        <div className="vstack gap-3">
-          <div className="d-flex align-items-center gap-3">
-            <img
-              src="/profileImages/handsome.jpg"
-              width="48"
-              height="48"
-              className="rounded-circle"
-              style={{ objectFit: "cover" }}
-            />
-            <span className="fw-semibold fs-5 text-white">
-              Chayanin Suatap 650610560
-            </span>
-          </div>
+        <PostOwnnerr {...postOwner}/>
 
-          <span className="text-white">
-            Quiz ง่ายจังเลยครับ ขอยาก ๆ กว่านี้ได้ไหม #261207
-          </span>
+        {comments.map((comment:CommentProps) => (<Commentt key = {comment.username} {...comment} />))}
 
-          <div className="d-flex align-items-center gap-1">
-            <img src="/like.svg" width={20}></img>
-            <span style={{ color: "#B0B3B8" }}>100 คน</span>
-          </div>
-          <hr className="m-0 border" />
-        </div>
-
-        {/* Comment Example */}
-        <div className="d-flex gap-2 my-2">
-          <img
-            src="/profileImages/lisa.jpg"
-            width="48"
-            height="48"
-            className="rounded-circle"
-            style={{ objectFit: "cover" }}
-          />
-          <div
-            className="rounded rounded-3 p-2"
-            style={{ backgroundColor: "#3A3B3C" }}
-          >
-            <span className="fw-semibold" style={{ color: "#E4E6EB" }}>
-              Lisa
-            </span>
-            <br />
-            <span style={{ color: "#E4E6EB" }}>จริงค่า</span>
-            <div className="d-flex align-items-center gap-1">
-              <img src="/like.svg" width={20}></img>
-              <span style={{ color: "#B0B3B8" }}>999 คน</span>
-            </div>
-          </div>
-        </div>
-
-        {/* Reply Example */}
-        <div className="d-flex gap-2 my-2 ps-5">
-          <img
-            src="/profileImages/puppy.jpg"
-            width="48"
-            height="48"
-            className="rounded-circle"
-            style={{ objectFit: "cover" }}
-          />
-          <div
-            className="rounded rounded-3 p-2"
-            style={{ backgroundColor: "#3A3B3C" }}
-          >
-            <span className="fw-semibold" style={{ color: "#E4E6EB" }}>
-              หมาน้อย
-            </span>
-            <br />
-            <span style={{ color: "#E4E6EB" }}>จริงค้าบบบบบบบบ</span>
-            <div className="d-flex align-items-center gap-1">
-              <img src="/like.svg" width={20}></img>
-              <span style={{ color: "#B0B3B8" }}>2 คน</span>
-            </div>
-          </div>
-        </div>
-
-        {/* map-loop render Comment component here */}
       </div>
     </div>
   );
--- src/components/Comment.tsx
+++ src/components/Comment.tsx
@@ -1,15 +1,45 @@
 "use client";
+
+import { CommentProps } from "@/libs/types"
+import { ReplyProps } from "@/libs/types"
+import Replyy from "@/components/Reply";
+
 export default function Comment({
   userImagePath,
   username,
   commentText,
   likeNum,
   replies,
-}) {
+}:CommentProps) {
   return (
     <div>
-      your code for Comment component here ...
-      {/* You can use map-loop to render Reply component here */}
+      {/* Comment Example */}
+      <div className="d-flex gap-2 my-2">
+          <img
+            src={userImagePath}
+            width="48"
+            height="48"
+            className="rounded-circle"
+            style={{ objectFit: "cover" }}
+          />
+          <div
+            className="rounded rounded-3 p-2"
+            style={{ backgroundColor: "#3A3B3C" }}
+          >
+            <span className="fw-semibold" style={{ color: "#E4E6EB" }}>
+              {username}
+            </span>
+            <br />
+            <span style={{ color: "#E4E6EB" }}>{commentText}</span>
+            { likeNum > 0 &&
+            <div className="d-flex align-items-center gap-1">
+              <img src="/like.svg" width={20}></img>
+              <span style={{ color: "#B0B3B8" }}>999 คน</span>
+            </div>
+            }
+          </div>
+        </div>
+        {replies.map((reply:ReplyProps) => (<Replyy key = {reply.username} {...reply} />))}
     </div>
   );
 }
